42e5f4f3a5e32f38916693263f8446a5c0ee8acc
Art2B <arthur.battut@gmail.com>
2018-04-11 22:54:35 +0200

    Update thank step

--- src/scripts/steps/Thanks.js
+++ src/scripts/steps/Thanks.js
@@ -6,19 +6,31 @@ import config from './../config'
 export default class Thanks extends Step {
   onFrame () {
     this.project.clear()
-    const yCenterOffset = 100
+    const yCenterOffset = 150
     const outroTopOptions = {
       ...config.textStyle,
-      point: new paper.Point(this.center.x, this.center.y - yCenterOffset),
+      fontSize: '142px',
+      point: new paper.Point(this.center.x, this.center.y),
       content: 'THANK YOU'
     }
+    const outroTopJokeOptions = {
+      ...config.textStyle,
+      fontSize: '18px',
+      fontWeight: 'normal',
+      point: new paper.Point(this.center.x + 450, this.center.y),
+      content: '(very much)'
+    }
+
     const outroBottomOptions = {
       ...config.textStyle,
+      fontSize: '48px',
+      fontWeight: 'normal',
       point: new paper.Point(this.center.x, this.center.y + yCenterOffset),
-      content: 'QUESTIONS ?'
+      content: 'any questions ?'
     }
 
     const outroTop = new paper.PointText(outroTopOptions)
+    const outroTopJoke = new paper.PointText(outroTopJokeOptions)
     const outroBottom = new paper.PointText(outroBottomOptions)
   }
 }
